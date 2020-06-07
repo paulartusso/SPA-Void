2653dd9a9b4c06ecfd2d6bed726c3d47575c6f6f
Paula Artusso <paulartusso@gmail.com>
2020-06-06 22:12:54 -0300

    js

--- void.js
+++ void.js
@@ -2,21 +2,21 @@ const cards = [
 
         {
             id: 1,
-            image: "pescado.jpg",
+            image: "../assets/images/pescado.jpg",
             type: "Remera",
-            title: "Reme Pescado Rabioso",
+            title: "Reme Pescado",
             price: 700
         },
         {
             id: 2,
-            image: "vinyl.jpg",
+            image: "../assets/images/octopus.jpg",
             type: "Remera",
             title: "Reme Vinilo",
             price: 700
         },
         {
             id: 3,
-            image: "octopus.jpg",
+            image: "../assets/images/vinyl.jpg",
             type: "Musculosa",
             title: "Musculosa Pulpo",
             price: 700 
@@ -24,15 +24,28 @@ const cards = [
     ];
 
 
-const createElement = () =>{
-    let cardContainer = document.getElementById("#card-container");
+const createElement = (item) =>{
+    let cardContainer = document.getElementById("card-container");
     let div = document.createElement("div");
+    div.setAttribute("class", "card")
     cardContainer.appendChild(div);
+
+    let img = document.createElement("img");
+    img.src = `img/${item.image}`;
+
     let title = document.createElement("h2");
-    let description = document.createElement("p");
+    title.innerText = item.title;
+    title.setAttribute("class", "h2");
+    
     let button = document.createElement("button");
-    div.appendChild(title);
-    div.appendChild(description);
-    div.appendChild(button);
-    button.innerText = "asdsfsdfsd";
+    button.setAttribute("class", "add-button");
+    button.innerText = "Agregar al carrito";
+    
+    div.appendChild(img);
+;   div.appendChild(title);
+    title.appendChild(button);
 }
+
+
+        
+    
