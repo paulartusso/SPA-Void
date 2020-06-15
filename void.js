659a38ebe27b8e4946215d628c510aab88896092
Paula Artusso <paulartusso@gmail.com>
2020-06-15 17:48:37 -0300

    arreglos css

--- void.js
+++ void.js
@@ -1,6 +1,5 @@
-const products ={
+const products = [
     
-    remeras: [
         {
             id: 1,
             image: "../assets/images/pescado.jpg",
@@ -22,45 +21,36 @@ const products ={
             title: "Musculosa Pulpo",
             price: 700 
         }
-    ],
-
-    varios: [
-        {
-            id: 4,
-            image: "../assets/images/mate.jpg",
-            title: "Mate Cortazar",
-            price: 400
-        },
-        {
-            id: 5,
-            image: "../assets/images/gorra.jpg",
-            title: "Gorra Ramas",
-            price: 550
-        }
-    ]
-};
+];
 
 const createFirstRow = (item) =>{
     let firstCardContainer = document.getElementById("first-card-container");
     let div = document.createElement("div");
     div.setAttribute("class", "card");
     firstCardContainer.appendChild(div);
-    
+   
     let img = document.createElement("img");
     img.src = `img/${item.image}`;
+    img.setAttribute("class", "card-img");
 
-    let title = document.createElement("h2");
-    title.innerText = item.title;
-    title.setAttribute("class", "h2");
+    let span = document.createElement("span");
+    span.setAttribute("class", "span-button-container");
+
     
     let button = document.createElement("button");
     button.setAttribute("class", "add-button");
     button.innerText = "Agregar al carrito";
     
+    let moreButton = document.createElement("button");
+    moreButton.setAttribute("class", "see-more-button");
+    moreButton.innerText = "+";
+    
     div.appendChild(img);
-    div.appendChild(title);
-    title.appendChild(button);
+    div.appendChild(span);
+    span.appendChild(button);
+    span.appendChild(moreButton);
 }
+
 
 
 const getFocus = () =>{
@@ -70,3 +60,16 @@ const getFocus = () =>{
 
 
 
+/**
+ * <3
+ * 
+ * Hoy le tenés que mandar evento onclick al boton de agregar al carrite
+ * y pushearle al mismo, pa crear la nueva lista (carrito).
+ * Después evento al boton con el changuito pa desplegar un modal que les 
+ * muestre a estos malditos consumistas en que innecesario producto están 
+ * gastando su dinero.
+ * THEN un contador en rojito para llevar el conteo de la cantidad de 
+ * items. Asumo que con un return listaCarrito(?) should be enough.
+ * El contadorcito posicionado según el carrite, ia tu sa, ahí arribita.
+
+ */
